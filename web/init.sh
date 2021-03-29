ea51c0e6d21e18e086d39451e6651af88e378d83
davert0 <78217283+davert0@users.noreply.github.com>
2021-03-29 17:47:49 +0300

    Update init.sh

--- web/init.sh
+++ web/init.sh
@@ -3,3 +3,9 @@ sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
 ﻿sudo /etc/init.d/mysql start﻿
+mkdir public
+mkdir uploads
+cd public
+mkdir img
+mkdir css
+mkdir js
