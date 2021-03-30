5c5633f2af0839207d68e9256459a6dea82a80b6
davert0 <ilya.v.sapunov@gmail.com>
2021-03-30 14:24:07 +0300

    blabla

--- web/hello.py
+++ web/hello.py
@@ -1,7 +1,7 @@
 def wsgi_application(environ, start_response):
     status = "200 OK"
     headers = [
-        ('Content-Type', 'text/plain')
+        ('Content-Type', 'text/plain; charset=utf-8')
     ]
     body = [bytes(i+'\n', 'ascii') for i in  environ['QUERY_STRING'].split('&')]
     start_response(status, headers)
