8b92c27353c7871c7341d0293177fbc34ecbd6ba
Ilya Sapunov <ilya.v.sapunov@gmail.com>
2021-04-13 08:11:40 +0300

    Добавлен скрипт подключения ДБ

--- web/ask/qa/models.py
+++ web/ask/qa/models.py
@@ -2,15 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Question(models.Model):
-    title = models.CharField(max_length=250)
-    text = models.TextField()
-    added_at = models.DateTimeField(auto_now_add=True)
-    rating = models.IntegerField(default=0)
-    author = models.ForeignKey(User)
-    likes = models.ManyToManyField(User, related_name='question_like_user')
-
-
 class QuestionManager(models.Manager):
     def new(self):
         return self.order_by('-added_at')
@@ -18,10 +9,30 @@ class QuestionManager(models.Manager):
         return self.order_by('-rating')
 
 
+class Question(models.Model):
+    title = models.CharField(max_length=250)
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(User)
+    likes = models.ManyToManyField(User, related_name='question_like_user')
+    objects = QuestionManager()
+
+    def __unicode__(self):
+        return self.text
+
+
+    def get_url(self):
+        return '/question/%d/' % self.id
+
+
+
+
+
 
 class Answer(models.Model):
     text = models.TextField()
-    added_at = models.DateTimeField()
+    added_at = models.DateTimeField(auto_now_add=True)
     question = models.ForeignKey(Question)
     author = models.ForeignKey(User)
 
