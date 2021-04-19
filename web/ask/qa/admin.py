a4bbbc1708792e0df7e4e7a6d592d82970ecfdba
Ilya Sapunov <ilya.v.sapunov@gmail.com>
2021-04-19 15:34:15 +0300

    добавлена поддержка юникода

--- web/ask/qa/admin.py
+++ web/ask/qa/admin.py
@@ -1,3 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 from django.contrib import admin
+from qa.models import Question, Answer
 
+admin.site.register(Question)
+admin.site.register(Answer)
 # Register your models here.
--- web/ask/qa/views.py
+++ web/ask/qa/views.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
 from django.http import HttpResponse, Http404, HttpResponseRedirect
